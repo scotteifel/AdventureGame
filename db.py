6454ad6e57893519a7ec84989c7875aac676c3bd
Scott <scott.eifel@hotmail.com>
2020-07-28 23:41:10 -0400

    Rooms have proper prompts, char. stats are recorded

--- db.py
+++ db.py
@@ -1,11 +1,7 @@
 import sqlite3, os
-<<<<<<< HEAD
 
 global CRNT_PLYR
 
-=======
-global CRNT_PLYR
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
 
 def create_db_table():
         #Create the database and map it to the project folder
@@ -15,7 +11,7 @@ def create_db_table():
         cur = conn.cursor()
         cur.execute('''CREATE TABLE IF NOT EXISTS character (name TEXT,
                         thirsty INTEGER, hungry INTEGER, tired INTEGER,
-                        room_tracker INTEGER)''')
+                        strength INTEGER, room_tracker INTEGER)''')
         conn.commit()
         cur.close()
         conn.close()
@@ -28,7 +24,7 @@ def add_char(name):
         conn = sqlite3.connect("main.db")
         cur = conn.cursor()
         cur.execute('''INSERT INTO character (name,thirsty,hungry,tired,
-                    room_tracker) VALUES (?,?,?,?,?)''', (name,1,1,0,0))
+            strength,room_tracker) VALUES (?,?,?,?,?,?)''', (name,1,1,0,0,0))
         conn.commit()
         cur.close()
         conn.close()
@@ -48,47 +44,82 @@ def edit_char_attr(**kwargs):
 
         conn = sqlite3.connect("main.db")
         cur = conn.cursor()
-<<<<<<< HEAD
-        cur.execute('''UPDATE character SET room_tracker = {x} WHERE
-                        name = (?)'''.format(x=kwargs['room']), (CRNT_PLYR,))
-=======
-        cur.execute('''UPDATE character SET room_tracker = {x}'''
-                        .format(x=kwargs["room"]))
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
-        conn.commit()
-        print("room is ",kwargs["room"])
 
-<<<<<<< HEAD
-        # if kwargs['tired'] == True:
-        #     cur.execute('''UPDATE character SET tired = 1 WHERE name =
-        #                     (?)''',(CRNT_PLYR,))
-=======
-        if kwargs['tired'] == True:
-            cur.execute('''UPDATE character SET tired = 1''')
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
+        if 'room' in kwargs:
 
-        conn.commit()
+            cur.execute('''UPDATE character SET room_tracker = {x} WHERE
+                            name = (?)'''.format(x=kwargs['room']), (CRNT_PLYR,))
+            conn.commit()
+
+        if 'thirsty' in kwargs:
+
+            if kwargs['thirsty'] == 1:
+                cur.execute('''UPDATE character SET thirsty = 1 WHERE name =
+                (?)''',(CRNT_PLYR,))
+            else:
+                 cur.execute('''UPDATE character SET thirsty = 0 WHERE name =
+                 (?)''',(CRNT_PLYR,))
+
+            conn.commit()
+
+        if 'hungry' in kwargs:
+
+            if kwargs['hungry'] == 1:
+                cur.execute('''UPDATE character SET hungry = 1 WHERE name =
+                (?)''',(CRNT_PLYR,))
+            else:
+                 cur.execute('''UPDATE character SET hungry = 0 WHERE name =
+                 (?)''',(CRNT_PLYR,))
+            conn.commit()
+
+        if 'tired' in kwargs:
+
+            if kwargs['tired'] == 1:
+                cur.execute('''UPDATE character SET tired = 1 WHERE name =
+                (?)''',(CRNT_PLYR,))
+            else:
+                 cur.execute('''UPDATE character SET tired = 0 WHERE name =
+                 (?)''',(CRNT_PLYR,))
+            conn.commit()
+
+        if 'strength' in kwargs:
+            #Put thirst, hunger, and tired to 0
+            cur.execute('''UPDATE character SET thirsty = 0, hungry = 0,
+                tired = 0 WHERE name = (?)''', (CRNT_PLYR,))
+            conn.commit()
+
+            #Add one strength
+            cur.execute('''SELECT strength from character WHERE name =
+            (?)''',(CRNT_PLYR,))
+            new_strength = cur.fetchone()[0]
+            new_strength+=1
+            print("New strength is ", new_strength)
+            cur.execute('''UPDATE character SET strength = (?) WHERE
+                name = (?)''',(new_strength, CRNT_PLYR))
+
+            conn.commit()
         cur.close()
-        print_char()
+        conn.close()
+
+def get_char_attr():
+
+        conn = sqlite3.connect("main.db")
+        cur = conn.cursor()
+        cur.execute('''SELECT * FROM character WHERE name = (?)''',
+                        (CRNT_PLYR,))
+
+        stats = cur.fetchone()
+        cur.close()
+        conn.close()
+        return stats
 
 def get_current_room():
-<<<<<<< HEAD
-    
+
         conn = sqlite3.connect("main.db")
         cur = conn.cursor()
         cur.execute('''SELECT room_tracker FROM character WHERE name =
                         (?)''',(CRNT_PLYR,))
         room = cur.fetchone()[0]
-=======
-        print_char()
-        print("Just printed")
-        conn = sqlite3.connect("main.db")
-        cur = conn.cursor()
-        cur.execute('''SELECT room_tracker WHERE name={tab}'''
-        .format(tab=CRNT_PLYR))
-        room = cur.fetchone()[0]
-        print(room, " This is from the db")
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
         cur.close()
         conn.close()
         return room
--- gui.py
+++ gui.py
@@ -2,19 +2,16 @@ import tkinter as tk
 from tkinter import ttk
 from ttkthemes import ThemedTk
 from db import (create_db_table, add_char, print_char, edit_char_attr,
-get_current_room)
+get_current_room, get_char_attr)
 
 
 # This is how the rooms are numbered in the game layout
-<<<<<<< HEAD
 # 0 is the entrance room
 # 1 is the bathroom
 # 2 is the workout room
 # 3 is the kitchen
-# 4 is the living room
-# 5 is the dining room
-=======
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
+# 4 is the dining room
+# 5 is the living room
 #        -------
 # ____  | 1 | 2 |
 # Entr. | 0 | 3 |
@@ -57,26 +54,22 @@ class Application(ttk.Frame):
         name = self.ask_name.get()
         if len(name) == 0:
             return
-        self.master.geometry("490x330")
+        self.master.geometry("510x400")
         self.lab.grid(padx=50,pady=50)
         add_char(name)
         self.enter.destroy()
         self.ask_name.destroy()
         self.lab.configure(text="Welcome to the main room {char}. \n\
-It has a high ceiling and some plants.  The bathroom is north, \nthe \
-kitchen is east.  There's a workout room to the south, and \nif you'd \
-like to leave, head back west and out the door.".format(char=name),width=65)
+It has a high ceiling and some plants.  The bathroom is North and \nthe \
+kitchen is to the East.  There's a workout room to the South, and \nif \
+you'd like to leave, head back West and out the door.  \nWhy not explore?"
+.format(char=name),width=65)
 
         #Draw house navigation
-<<<<<<< HEAD
         self.north = ttk.Button(self.master,text='North',
                                     command=self.go_north)
         self.south = ttk.Button(self.master,text='South',
                                     command=self.go_south)
-=======
-        self.north = ttk.Button(self.master,text='North',command=self.go_north)
-        self.south = ttk.Button(self.master,text='South',command=self.go_south)
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
         self.east = ttk.Button(self.master,text='East',command=self.go_east)
         self.west = ttk.Button(self.master,text='West',
                     command=self.go_west)
@@ -91,7 +84,6 @@ like to leave, head back west and out the door.".format(char=name),width=65)
     def go_north(self):
 
         room = get_current_room()
-<<<<<<< HEAD
 
         if room == 0:
             self.room_prompt(1)
@@ -99,15 +91,6 @@ like to leave, head back west and out the door.".format(char=name),width=65)
             kw = {"tired":True, "room":"1"}
             edit_char_attr(**kw)
 
-=======
-        kw = {'tired':"True", "room":"1"}
-        edit_char_attr(**kw)
-        print(type(room),room,"   That is room")
-        if room == 0:
-            print("ROOM IS 0")
-            char_inf = {'room':1}
-            edit_char_attr(**char_inf)
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
             self.north['state'] = 'disabled'
             self.west['state'] = 'disabled'
 
@@ -136,69 +119,37 @@ like to leave, head back west and out the door.".format(char=name),width=65)
 
         self.south['state'] = 'normal'
 
-        elif room == 3:
-            char_inf = {'room':'2'}
-            edit_char_attr(**char_inf)
-
-            self.north['state'] = 'disabled'
-            self.east['state'] = 'disabled'
-
-        elif room == 5:
-            char_inf = {'room':0}
-            edit_char_attr(**char_inf)
-
-            self.west['state'] = 'normal'
-            self.south['state'] = 'normal'
-
-        elif room == 4:
-            char_inf = {'room':3}
-            edit_char_attr(**char_inf)
-
-            self.south['state'] = 'normal'
-
 
     def go_east(self):
         room = get_current_room()
 
         if room == 1:
-<<<<<<< HEAD
             self.room_prompt(2)
 
-=======
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
             char_inf = {'room':2}
             edit_char_attr(**char_inf)
 
         elif room == 0:
-<<<<<<< HEAD
+            print("Room is 0")
             self.room_prompt(3)
 
-=======
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
             char_inf = {'room':3}
             edit_char_attr(**char_inf)
 
         elif room == 5:
-<<<<<<< HEAD
             self.room_prompt(4)
 
-=======
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
             char_inf = {'room':4}
             edit_char_attr(**char_inf)
 
         self.east['state'] = 'disabled'
-<<<<<<< HEAD
         self.west['state'] = 'normal'
-=======
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
 
 
     def go_south(self):
         room = get_current_room()
 
         if room == 1:
-<<<<<<< HEAD
             self.room_prompt(0)
 
             char_inf = {'room':0}
@@ -215,35 +166,19 @@ like to leave, head back west and out the door.".format(char=name),width=65)
         elif room == 0:
             self.room_prompt(5)
 
-=======
-            char_inf = {'room':0}
-            edit_char_attr(**char_inf)
-
-        elif room == 2:
-            char_inf = {'room':3}
-            edit_char_attr(**char_inf)
-
-        elif room == 0:
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
             char_inf = {'room':5}
             edit_char_attr(**char_inf)
 
             self.south['state'] = 'disabled'
 
         elif room == 3:
-<<<<<<< HEAD
             self.room_prompt(4)
 
-=======
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
             char_inf = {'room':4}
             edit_char_attr(**char_inf)
 
             self.south['state'] = 'disabled'
-<<<<<<< HEAD
         self.north['state'] = 'normal'
-=======
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
 
 
 
@@ -254,7 +189,6 @@ like to leave, head back west and out the door.".format(char=name),width=65)
             self.master.destroy()
 
         elif room == 2:
-<<<<<<< HEAD
             self.room_prompt(1)
 
             char_inf = {'room':1}
@@ -264,18 +198,10 @@ like to leave, head back west and out the door.".format(char=name),width=65)
         elif room == 3:
             self.room_prompt(0)
 
-
-=======
-            char_inf = {'room':1}
-            edit_char_attr(**char_inf)
-
-        elif room == 3:
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
             char_inf = {'room':0}
             edit_char_attr(**char_inf)
 
         elif room == 4:
-<<<<<<< HEAD
             self.room_prompt(5)
 
             char_inf = {'room':5}
@@ -286,32 +212,135 @@ like to leave, head back west and out the door.".format(char=name),width=65)
 
 
     def room_prompt(self,_room):
+        stats = get_char_attr()
+        #Values of stats response by index order are:
+        # 0 = name
+        # 1 = thirsty (1 is yes, 0 is no)
+        # 2 = hungry (1 is yes, 0 is no)
+        # 3 = tired (1 is yes, 0 is no)
+        # 4 = strength level
+        # 5 = current room
 
+########################     ENTRANCE     ##################################
         if _room == 0:
-            self.lab.configure(text="You're in the entrance hall")
+            print("ENTRANCE ROOM")
+            self.lab.configure(text="You're back in the entrance hall.  Go \
+west to head out the door.")
 
+########################     RESTROOM     ##################################
         elif _room == 1:
-            self.lab.configure(text="You washed up in the restroom.  Wow \
-what a\n nice smelling soap they have.  Now you can go \nto the living room \
+
+            #Check the player's thirst
+            if stats[1] == 0:
+
+                self.lab.configure(text="You washed up in the restroom. \
+You took a sip from a dixie cup and are refreshed.", justify=tk.CENTER)
+
+            else:
+                self.lab.configure(text="You washed up in the restroom.  Wow \
+what a nice \nsmelling soap they have.  Now you can go \nto the living room \
 to the east or head back to the \nmain room.",justify=tk.CENTER)
 
+########################     WORKOUT ROOM     #############################
         elif _room == 2:
-            self.lab.configure(text="You're in the workout room")
-=======
-            char_inf = {'room':5}
-            edit_char_attr(**char_inf)
+            print("ROOM 2 WORKOUT ROOM")
 
-        self.east['state'] = 'disabled'
->>>>>>> 00de1221adac94b978e294ef7c8db8bc11bc3a12
+            #Check if the player is tired
+            if stats[3] == 0:
+                print("The player's tired")
 
+                self.lab.configure(text="You're too tired to lift and run, \
+after you rest you should be \ngood to go though.  If you can find a nice \
+chair to sink\n into.  That should do the trick.")
+
+            #Check the player's hunger
+            elif stats[2] == 0:
+                self.lab.configure(text="Mm, you need a snack for some \
+strength before you hit the\n iron.  Try looking around for something to \
+munch on.")
+
+
+            #Check if the player's thirsty
+            elif stats[1] == 0:
+                 self.lab.configure(text="Here's the workout room, but ahh, \
+you're parched and there's no \nwatertank in here.  You need a refreshment \
+before you get going with any workouts.  Search some other rooms for \
+something to help with that")
+
+            else:
+                self.lab.configure(text="You're in the exercise room now and \
+wow, look at you go.  \nWhoa, did you stretch?  Alright, alright!  You are\n \
+really doing a great job.  That definately tired you \nout.  You could use \
+something to drink, and maybe a snack, \ntoo.  if you want to workout some \
+more.  The kitchen might be a \ngood place make your way to.")
+
+            kw = {'thirsty': 0,'hungry':0,'tired':0,'strength':1}
+            edit_char_attr(**kw)
+
+########################    KITCHEN     #####################################
         elif _room == 3:
-            self.lab.configure(text="You're in the kitchen.  Wow a chicken!")
+            print("IN ROOM 3, KITCHEN")
 
+            #Check thirst
+            if stats[1] == 0:
+                print("Yes I am thirsty.")
+
+                self.lab.configure(text="You're in the kitchen.  This is a \
+nice area with marble countertops\n and recently updated cabinets.  You \
+drink a glass of nice cold water\n and are refreshed.")
+
+                kw = {'thirsty': 1}
+                edit_char_attr(**kw)
+
+            else:
+                print("No I'm not thirsty.")
+
+                self.lab.configure(text="You're in the kitchen.  This is a \
+nice area with marble countertops\n and recently updated cabinets.  You're \
+satisfied so \nthere's no need for a drink of water for ya now.")
+
+########################    DININGROOM     ################################
         elif _room == 4:
-            self.lab.configure(text="You're in the diningroom")
+            print("DINING ROOM")
+            #Check hunger
+            if stats[2] == 0:
+                print("Yes I'm hungry")
 
+                self.lab.configure(text="You're in the diningroom.  MMM \
+there's freshly cooked ham\n on a platter for you to enjoy.  You grab a \
+slice,\n and a piece of the pineapple that's on it too.  Mmm mm,\n was that \
+satisfying.")
+
+                kw = {'hungry': 1}
+                edit_char_attr(**kw)
+
+            else:
+                print("Not hungry")
+
+                self.lab.configure(text="You're in the diningroom now and \
+there's a delicious ham set out.\n  You're not hungry, but maybe in a bit \
+you can have a piece.")
+
+#####################      LIVINGROOM      ##################################
         elif _room == 5:
-            self.lab.configure(text="You're in the livingroom")
+            print("LIVING ROOM")
+
+            #Check the if the player's tired
+            if stats[3] == 0:
+
+                self.lab.configure(text="Your in the livingroom.  It's \
+really nice with great\n furniture and tall drapes and the sunshine is really \
+streaming in\n through that window.  Ohh what a nice recliner there\n is in \
+the corner!  You sit down, play a little Sudoku,\n and rest for a bit.  \
+Whoa, where did the time\n go??  You dozed off for a minute!  \nA bite \
+to eat seems to be in order now.\n  Where's that diningroom?")
+                kw = {'hungry': 0, 'tired':1}
+                edit_char_attr(**kw)
+            else:
+
+                self.lab.configure(text="Your in the livingroom.  It's \
+really nice with great furniture and tall\n drapes.  The sunshine is really \
+streaming in through that window.")
 
 
 def main():
@@ -321,7 +350,6 @@ def main():
     root.title('AdventureGame')
     app = Application(master=root)
     app.mainloop()
-
 
 
 if __name__ == '__main__':
